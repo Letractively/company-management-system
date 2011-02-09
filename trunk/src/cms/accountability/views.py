938f92f1f2c178a5905143568ba0c81e3cb63ee7
MICHAELLAWS@gmail.com <MICHAELLAWS@gmail.com@cb1e0fea-bcdf-831c-43be-d7980feae904>
2011-02-09 13:52:03 +0000

    refractor

--- trunk/src/cms/accountability/views.py
+++ trunk/src/cms/accountability/views.py
@@ -1,5 +1,9 @@
-from cms.Accountability.models import Event
-from cms.Accountability.models import Attendance
+# views.py
+# Author: Dimitri Hatley
+# Editor: Michael Laws
+
+from cms.accountability.models import Event
+from cms.accountability.models import Attendance
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
 
