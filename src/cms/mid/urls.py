3bea468709248e7bb3aa06720dab38a34505e014
MICHAELLAWS <michaellaws@gmail.com>
2011-02-12 22:00:03 +0000

    urls?

--- src/cms/mid/urls.py
+++ src/cms/mid/urls.py
@@ -11,6 +11,7 @@ urlpatterns = patterns('mid.views',
     # (r'^cms/', include('cms.foo.urls')),
     
     (r'^$','loginPage'),
+    (r'^login$', 'log_in'),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     (r'^admin/doc/', include('django.contrib.admindocs.urls')),
