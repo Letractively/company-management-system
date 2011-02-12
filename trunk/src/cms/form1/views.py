98720f52b07a0df9bbf251758fc29ed57dfb4a63
MICHAELLAWS@gmail.com <MICHAELLAWS@gmail.com@cb1e0fea-bcdf-831c-43be-d7980feae904>
2011-02-12 22:48:35 +0000

    form1?

--- trunk/src/cms/form1/views.py
+++ trunk/src/cms/form1/views.py
@@ -1,1 +1,24 @@
-# Create your views here.
+#form1 views.py
+# Author: Dimitri Hatley
+# Editor: Michael Laws
+
+
+# note to dimitri:  i wrote this to try to figure out some url nonsense.
+#  none of this is well thought out or close to permanent.  feel free to delete all of it and start over.
+
+from mid.models import Mid
+from form1.models import Form1
+from django.http import HttpResponse
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+
+
+def createNewForm1(request):
+    
+    rForm1 = Form1.objects.REASON_CHOICES
+    
+    return render_to_response('form1/createNewForm1.html', { 'reasons' : rForm1})
+ 
+ 
+def reviewForm1(request):
+    return render_to_response('form1/reviewForm1.html')
