40b5c9beea7f5366e5a3b74be95a0042ec9fd613
ryanrabe2011 <ryanrabe2011@gmail.com>
2011-02-24 20:18:42 +0000

    static serving of pages

--- src/cms/urls.py
+++ src/cms/urls.py
@@ -5,6 +5,8 @@
 from django.conf.urls.defaults import *
 # admin enabled
 from django.contrib import admin
+from django.views.static import *
+from django.conf import settings
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -32,8 +34,12 @@ urlpatterns = patterns('',
     
 
     # Uncomment the admin/doc line below to enable admin documentation:
-     (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # enabled the admin
-     (r'^admin/', include(admin.site.urls)),
+    (r'^admin/', include(admin.site.urls)),
+    
+    # static content
+    (r'media/(?P<path>.*)$','django.views.static.serve',
+    {'document_root': settings.MEDIA_ROOT}),
 )
