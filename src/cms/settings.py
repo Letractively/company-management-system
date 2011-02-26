cd6ddfa10fde108d15f235e75c26705c672d7076
MICHAELLAWS <michaellaws@gmail.com>
2011-02-26 23:01:21 +0000

    media fix

--- src/cms/settings.py
+++ src/cms/settings.py
@@ -67,7 +67,7 @@ MEDIA_ROOT = '/var/www/media/'
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = '/cms/'
+MEDIA_URL = '/media/cms/'
 
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
