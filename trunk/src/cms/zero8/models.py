879394926246fc129c00861b78cb2ff6cbe27c7f
MICHAELLAWS@gmail.com <MICHAELLAWS@gmail.com@cb1e0fea-bcdf-831c-43be-d7980feae904>
2011-02-27 20:34:20 +0000

    finally figured out how to return date strings.

--- trunk/src/cms/zero8/models.py
+++ trunk/src/cms/zero8/models.py
@@ -22,7 +22,7 @@ class Zero8(models.Model):
     workOrderClosed = models.IntegerField()
     workOrderOverdue = models.IntegerField()
     def __unicode__(self):
-        return self.reportDate
+        return unicode(self.reportDate)
     
 class SignificantEvents(models.Model):
     zero8 = models.ForeignKey(Zero8)
