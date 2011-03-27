bf8c5e84d3f6d823dd0d51fd58cf9541b57ac3ae
MICHAELLAWS@gmail.com <MICHAELLAWS@gmail.com@cb1e0fea-bcdf-831c-43be-d7980feae904>
2011-03-27 03:32:08 +0000

    form1 done??

--- trunk/src/cms/form1/urls.py
+++ trunk/src/cms/form1/urls.py
@@ -8,10 +8,10 @@ urlpatterns = patterns('form1.views',
 
     url(r'^$', 'form1', name = "form1"),
     (r'^/$', 'form1'),
-    (r'formOneView', 'formOneView'),
-    (r'formOneSubmit$', 'formOneSubmit'),
-    url(r'formOneSelect$', 'formOneSelect', name = "formOneSelect"),
-    (r'formOneSave$', 'formOneSave'),
+    (r'form1View', 'form1View'),
+    (r'form1Submit$', 'form1Submit'),
+    url(r'form1Select$', 'form1Select', name = "form1Select"),
+    (r'form1Save$', 'form1Save'),
     
     # Uncomment the admin/doc line below to enable admin documentation:    
     (r'^admin/doc/', include('django.contrib.admindocs.urls')),
