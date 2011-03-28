07eb421ae3dbf5762cf9de54664d5729c5469c35
marinemike1985 <marinemike1985@cb1e0fea-bcdf-831c-43be-d7980feae904>
2011-03-28 03:34:52 +0000

    This is just the urls for the AcYear.

--- trunk/src/cms/companywatch/urls.py
+++ trunk/src/cms/companywatch/urls.py
@@ -4,11 +4,14 @@ from django.conf.urls.defaults import *
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = patterns('companywatch.views',
 
     # put url's to all the apps in this folder.
 
-    # (r'^cms/', include('cms.foo.urls')),
+    url(r'^$', 'AcYear', name = "AcYear"),
+    (r'^/$', 'AcYear'),
+    (r'AcYearSubmit$', 'AcYearSubmit'),
+    (r'AcYearView', 'AcYearView'),
 
     # Uncomment the admin/doc line below to enable admin documentation:
      (r'^admin/doc/', include('django.contrib.admindocs.urls')),
