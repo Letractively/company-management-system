f0b6b2fb2c7be29ff09abad822f4c528de6b8c48
marinemike1985 <marinemike1985@gmail.com>
2011-04-12 18:32:28 +0000

    This is from the linode

--- src/cms/unitleader_script.py
+++ src/cms/unitleader_script.py
@@ -1,4 +1,21 @@
 from django.core.management import setup_environ
-from mysite import settings
-
+import settings
 setup_environ(settings)
+
+from unit.models import Unit
+from unit.models import UnitLeader
+
+leaders = UnitLeader.objects.all()
+
+for leader in leaders:
+	print u'%s %s %s' % (leader,leader.unitNumber,leader.billet)
+	
+	if leader.billet == 'BO':
+		unit = Unit.objects.get(battalion = leader.unitNumber,company ='0')
+		leader.unit_id = unit.id
+		leader.save()
+	else:
+		unit = Unit.objects.get(company = leader.unitNumber)
+		leader.unit_id = unit.id
+		leader.save()
+
