3b01b40005e0bed425ce247b0869ec396c4cf1ba
marinemike1985 <marinemike1985@gmail.com>
2011-04-13 01:49:09 +0000

    This script is actually quite a bit shorter than the last one. but this should work better and cleaner.

--- src/cms/user_script.py
+++ src/cms/user_script.py
@@ -1,6 +1,11 @@
 from django.core.management import setup_environ
-from mysite import settings
-
+import settings
 setup_environ(settings)
 
+from django.contrib.auth.models import User
+from mid.models import Mid
 
+mids = Mid.objects.all()
+
+for mid in mids:
+    user = User.objects.create_user(mid.fName + ' '+ mid.LName,'m'+mid.alpha+'@usna.edu','1')
